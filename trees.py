f465c2a4ab6c3c272a4cd6bc656383fb736695b1
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-07-10 22:09:55 +0200

    Adjust Tree Location to Vertices from Terrain

--- trees.py
+++ trees.py
@@ -28,12 +28,6 @@ class OBJECT_OT_add_trees(bpy.types.Operator):
         default=1
     )
 
-    GRID_LENGTH: bpy.props.IntProperty(
-        name="Grid Size",
-        description="Determine the size of the Grid-Area",
-        default=50
-    )
-
     def getRandom(self, min: float, max: float) -> float:
         seed = datetime.now().timestamp()
         random.seed(seed)
@@ -88,61 +82,68 @@ class OBJECT_OT_add_trees(bpy.types.Operator):
         return material  # Return Material
 
     def create_trees(self):
-        for i in range(self.NUMBER_TREES):
-            # Variables
-            length = (
-                self.getRandom(0.5, 1.2),
-                self.getRandom(0.5, 0.9),
-                0.5,
-                0.1
-            )
-            downAngle = (
-                0,
-                self.getRandom(25, 35),
-                55,
-                30
-            )
-            leaves = self.getRandom(300, 400)
-            branches = (
-                0,
-                int(self.getRandom(50, 70)),
-                10,
-                1
-            )
-            segSplits = (0.1, self.getRandom(0.5, 0.7), 0.2, 0)
+        # Extract Data from Terrain
+        terrain: bpy.types.Object = bpy.data.objects.get('Terrain-Plane')
+        if terrain:
+            # Prepare Materials
+            WOOD_MAT = self.create_wood_material()
+            LEAF_MAT = self.create_leaf_material()
 
-            # Create Tree based on "Sapling Tree Gen"
-            bpy.ops.curve.tree_add(
-                length=length,
-                downAngle=downAngle,
-                leaves=leaves,
-                branches=branches,
-                segSplits=segSplits,
-                do_update=True, bevel=True, prune=False, showLeaves=True, useArm=False, seed=0, handleType='0', levels=2, lengthV=(0, 0.1, 0, 0), taperCrown=0.5, curveRes=(8, 5, 3, 1), curve=(0, -15, 0, 0), curveV=(20, 50, 75, 0), curveBack=(0, 0, 0, 0), baseSplits=3, splitByLen=True, rMode='rotate', splitAngle=(18, 18, 22, 0), splitAngleV=(5, 5, 5, 0), scale=5, scaleV=2, attractUp=(3.5, -1.89984, 0, 0), attractOut=(0, 0.8, 0, 0), shape='7', shapeS='10', customShape=(0.5, 1, 0.3, 0.5), branchDist=1.5, nrings=0, baseSize=0.3, baseSize_s=0.16, splitHeight=0.2, splitBias=0.55, ratio=0.015, minRadius=0.0015, closeTip=False, rootFlare=1, autoTaper=True, taper=(
-                    1, 1, 1, 1), radiusTweak=(1, 1, 1, 1), ratioPower=1.2, downAngleV=(0, 10, 10, 10), useOldDownAngle=True, useParentAngle=True, rotate=(99.5, 137.5, 137.5, 137.5), rotateV=(15, 0, 0, 0), scale0=1, scaleV0=0.1, pruneWidth=0.34, pruneBase=0.12, pruneWidthPeak=0.5, prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=0.75, leafDownAngle=30, leafDownAngleV=-10, leafRotate=137.5, leafRotateV=15, leafScale=0.3, leafScaleX=0.2, leafScaleT=0.1, leafScaleV=1.0, leafShape='hex', bend=0, leafangle=-12, horzLeaves=True, leafDist='6', bevelRes=1, resU=4, armAnim=False, previewArm=False, leafAnim=False, frameRate=1, loopFrames=0, wind=1, gust=1, gustF=0.075, af1=1, af2=1, af3=4, makeMesh=False, armLevels=2, boneStep=(1, 1, 1, 1))
+            # Extract Vertices from Terrain
+            VERTS = [(terrain.matrix_world @ v.co)
+                     for v in terrain.data.vertices]
 
-            # Create & append Materials
-            mat_wood = self.create_wood_material()
-            mat_leaf = self.create_leaf_material()
+            # Create Trees
+            for i in range(self.NUMBER_TREES):
+                # Variables
+                length = (
+                    self.getRandom(0.6, 1.1),
+                    self.getRandom(0.6, 0.9),
+                    0.5,
+                    0.1
+                )
+                downAngle = (
+                    0,
+                    self.getRandom(25, 35),
+                    55,
+                    30
+                )
+                leaves = self.getRandom(100, 200)
+                branches = (
+                    0,
+                    int(self.getRandom(50, 70)),
+                    10,
+                    1
+                )
+                segSplits = (0.1, self.getRandom(0.5, 0.7), 0.2, 0)
 
-            # Context for Tree & Leaf
-            tree: bpy.types.Object
-            tree = bpy.data.objects.get('tree')
-            if tree:
-              tree.name = 'tree.' + str(i)
+                # Create Tree based on "Sapling Tree Gen"
+                bpy.ops.curve.tree_add(
+                    length=length,
+                    downAngle=downAngle,
+                    leaves=leaves,
+                    branches=branches,
+                    segSplits=segSplits,
+                    do_update=True, bevel=True, prune=False, showLeaves=True, useArm=False, seed=0, handleType='0', levels=2, lengthV=(0, 0.1, 0, 0), taperCrown=0.5, curveRes=(8, 5, 3, 1), curve=(0, -15, 0, 0), curveV=(20, 50, 75, 0), curveBack=(0, 0, 0, 0), baseSplits=3, splitByLen=True, rMode='rotate', splitAngle=(18, 18, 22, 0), splitAngleV=(5, 5, 5, 0), scale=5, scaleV=2, attractUp=(3.5, -1.89984, 0, 0), attractOut=(0, 0.8, 0, 0), shape='7', shapeS='10', customShape=(0.5, 1, 0.3, 0.5), branchDist=1.5, nrings=0, baseSize=0.3, baseSize_s=0.16, splitHeight=0.2, splitBias=0.55, ratio=0.015, minRadius=0.0015, closeTip=False, rootFlare=1, autoTaper=True, taper=(
+                        1, 1, 1, 1), radiusTweak=(1, 1, 1, 1), ratioPower=1.2, downAngleV=(0, 10, 10, 10), useOldDownAngle=True, useParentAngle=True, rotate=(99.5, 137.5, 137.5, 137.5), rotateV=(15, 0, 0, 0), scale0=1, scaleV0=0.1, pruneWidth=0.34, pruneBase=0.12, pruneWidthPeak=0.5, prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=0.75, leafDownAngle=30, leafDownAngleV=-10, leafRotate=137.5, leafRotateV=15, leafScale=0.3, leafScaleX=0.2, leafScaleT=0.1, leafScaleV=1.0, leafShape='hex', bend=0, leafangle=-12, horzLeaves=True, leafDist='6', bevelRes=1, resU=4, armAnim=False, previewArm=False, leafAnim=False, frameRate=1, loopFrames=0, wind=1, gust=1, gustF=0.075, af1=1, af2=1, af3=4, makeMesh=False, armLevels=2, boneStep=(1, 1, 1, 1))
 
-            leaf = tree.children[0]
+                # Context for Tree & Leaf
+                tree: bpy.types.Object
+                tree = bpy.data.objects.get('tree')
+                if tree:
+                    tree.name = 'tree.' + str(i)
 
-            # Add Materials
-            tree.data.materials.append(mat_wood)
-            leaf.data.materials.append(mat_leaf)
+                leaf = tree.children[0]
 
-            # Set Location
-            tree.location = (
-                self.getRandom(0, self.GRID_LENGTH),
-                self.getRandom(0, self.GRID_LENGTH),
-                0.0
-            )
+                # Add Materials
+                tree.data.materials.append(WOOD_MAT)
+                leaf.data.materials.append(LEAF_MAT)
+
+                # Set Location
+                vert = VERTS[int(self.getRandom(0, len(VERTS)))]
+                tree.location = (vert[0], vert[1], vert[2])
+        else:
+            print('Add Terrain first')
 
     def execute(self, context):
         self.create_trees()
