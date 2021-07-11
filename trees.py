fc89450aef1e29bdf7f3f49b7bdbc629220c01e4
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-07-11 12:49:49 +0200

    Improve Tree Structure, Perormance & Documentation

--- trees.py
+++ trees.py
@@ -1,4 +1,4 @@
-import math
+from math import radians
 import random
 import typing
 import bpy
@@ -97,35 +97,19 @@ class OBJECT_OT_add_trees(bpy.types.Operator):
             for i in range(self.NUMBER_TREES):
                 # Variables
                 length = (
-                    self.getRandom(0.6, 1.1),
-                    self.getRandom(0.6, 0.9),
-                    0.5,
-                    0.1
+                    self.getRandom(0.45, 0.55),
+                    self.getRandom(0.25, 0.35),
+                    0.0,
+                    0.0
                 )
-                downAngle = (
-                    0,
-                    self.getRandom(25, 35),
-                    55,
-                    30
-                )
-                leaves = self.getRandom(100, 200)
-                branches = (
-                    0,
-                    int(self.getRandom(50, 70)),
-                    10,
-                    1
-                )
-                segSplits = (0.1, self.getRandom(0.5, 0.7), 0.2, 0)
+                splitHeight = self.getRandom(0.2, 1.0)
 
                 # Create Tree based on "Sapling Tree Gen"
                 bpy.ops.curve.tree_add(
+                    splitHeight=splitHeight,
                     length=length,
-                    downAngle=downAngle,
-                    leaves=leaves,
-                    branches=branches,
-                    segSplits=segSplits,
-                    do_update=True, bevel=True, prune=False, showLeaves=True, useArm=False, seed=0, handleType='0', levels=2, lengthV=(0, 0.1, 0, 0), taperCrown=0.5, curveRes=(8, 5, 3, 1), curve=(0, -15, 0, 0), curveV=(20, 50, 75, 0), curveBack=(0, 0, 0, 0), baseSplits=3, splitByLen=True, rMode='rotate', splitAngle=(18, 18, 22, 0), splitAngleV=(5, 5, 5, 0), scale=5, scaleV=2, attractUp=(3.5, -1.89984, 0, 0), attractOut=(0, 0.8, 0, 0), shape='7', shapeS='10', customShape=(0.5, 1, 0.3, 0.5), branchDist=1.5, nrings=0, baseSize=0.3, baseSize_s=0.16, splitHeight=0.2, splitBias=0.55, ratio=0.015, minRadius=0.0015, closeTip=False, rootFlare=1, autoTaper=True, taper=(
-                        1, 1, 1, 1), radiusTweak=(1, 1, 1, 1), ratioPower=1.2, downAngleV=(0, 10, 10, 10), useOldDownAngle=True, useParentAngle=True, rotate=(99.5, 137.5, 137.5, 137.5), rotateV=(15, 0, 0, 0), scale0=1, scaleV0=0.1, pruneWidth=0.34, pruneBase=0.12, pruneWidthPeak=0.5, prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=0.75, leafDownAngle=30, leafDownAngleV=-10, leafRotate=137.5, leafRotateV=15, leafScale=0.3, leafScaleX=0.2, leafScaleT=0.1, leafScaleV=1.0, leafShape='hex', bend=0, leafangle=-12, horzLeaves=True, leafDist='6', bevelRes=1, resU=4, armAnim=False, previewArm=False, leafAnim=False, frameRate=1, loopFrames=0, wind=1, gust=1, gustF=0.075, af1=1, af2=1, af3=4, makeMesh=False, armLevels=2, boneStep=(1, 1, 1, 1))
+                    do_update=True, chooseSet='0', bevel=True, prune=False, showLeaves=True, useArm=False, seed=0, handleType='0', levels=2, lengthV=(0, 0, 0, 0), taperCrown=0, branches=(0, 25, 0, 0), curveRes=(16, 5, 3, 1), curve=(0, -20, -20, 0), curveV=(400, 150, 100, 0), curveBack=(0, 0, 0, 0), baseSplits=0, segSplits=(0.25, 3, 0, 0), splitByLen=True, rMode='rotate', splitAngle=(15, 20, 25, 0), splitAngleV=(5, 5, 0, 0), scale=6, scaleV=2, attractUp=(0, -0.35, -0.2, 0), attractOut=(0, 0.75, 0.25, 0), shape='8', shapeS='10', customShape=(0.7, 1, 0.2, 0.8), branchDist=1, nrings=0, baseSize=0.4, baseSize_s=0.25, splitBias=0, ratio=0.02, minRadius=0.0015, closeTip=False, rootFlare=1, autoTaper=True, taper=(
+                        1, 1, 1, 1), radiusTweak=(1, 1, 1, 1), ratioPower=1.25, downAngle=(90, 90, 30, 30), downAngleV=(0, 90, 15, 10), useOldDownAngle=False, useParentAngle=False, rotate=(99.5, 137.5, 137.5, 137.5), rotateV=(15, 0, 0, 0), scale0=1, scaleV0=0.1, pruneWidth=0.4, pruneBase=0.3, pruneWidthPeak=0.6, prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=1, leaves=-5, leafDownAngle=45, leafDownAngleV=10, leafRotate=137.5, leafRotateV=0, leafScale=0.17, leafScaleX=0.2, leafScaleT=-0.5, leafScaleV=0, leafShape='hex', bend=0, leafangle=-10, horzLeaves=True, leafDist='6', bevelRes=0, resU=1, armAnim=False, previewArm=False, leafAnim=False, frameRate=1, loopFrames=0, wind=1, gust=1, gustF=0.075, af1=1, af2=1, af3=4, makeMesh=False, armLevels=2, boneStep=(1, 1, 1, 1))
 
                 # Context for Tree & Leaf
                 tree: bpy.types.Object
@@ -142,6 +126,8 @@ class OBJECT_OT_add_trees(bpy.types.Operator):
                 # Set Location
                 vert = VERTS[int(self.getRandom(0, len(VERTS)))]
                 tree.location = (vert[0], vert[1], vert[2])
+
+                tree.rotation_euler = (0, 0, radians(self.getRandom(0, 360)))
         else:
             print('Add Terrain first')
 
